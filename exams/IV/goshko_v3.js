e1fd75097457ed52263d728073760d0e3869fad5
Margarita Ignatieva <margaret.vegan@gmail.com>
2016-01-27 16:44:36 +0200

    simplified goshko

--- exams/IV/goshko_v3.js
+++ exams/IV/goshko_v3.js
@@ -5,29 +5,24 @@ function goshko(arr){
 	cells = [],
 	position = [0, 0],
 	map = {
-		"right": [1, 0],
-		"left": [-1, 0],
-		"up": [0, -1],
-		"down": [0, 1]
+		"right": [0, 1],
+		"left": [0, -1],
+		"up": [-1, 0],
+		"down": [1, 0]
 	},
-
 	output = {
-		"{&}":0,
-		"{*}":0,
-		"{#}":0,
-		"{!}":0,
-		"wallHits": 0
-	}
-	overallRegx = /{&}|({\*})|({#})|{!}/g;
+		"&":0,
+		"*":0,
+		"#":0,
+		"!":0,
+		"wall hits": 0
+	},
+	overallRegx = /{[*&!#]}/g;
 	
-	
-
-	//populate garden
 	for (var i = 1; i < arr.length; i ++){
 		var row = arr[i].split(",");
 		garden.push(row)
 	}
-
 	function move(direction, position){
 		var wasMove = false;
 		if ( position[0] + map[direction][0] >= 0 && position[0] + map[direction][0] < garden.length 
@@ -35,24 +30,21 @@ function goshko(arr){
 			position[0] += map[direction][0];
 			position[1] += map[direction][1];
 			wasMove = true;
-		
 		} 
 		return [position, wasMove];
 	}
-console.log("directions ", directions)
+// console.log("directions ", directions)
 	for (var idx in directions) {
 		var direction = directions[idx].trim();
-		console.log(position)
+		// console.log(position)
 		var go = move(direction, position);
 		if ( go[1] ) {
 			// console.log(garden[go[0][1]][go[0][0]]);
-			cells.push(garden[go[0][1]][go[0][0]])
+			cells.push(garden[go[0][0]][go[0][1]])
 		} else {
-			console.log(idx, "hit wall")
-			++output.wallHits;
+			// console.log(idx, "hit wall")
+			++output['wall hits'];
 		}
-		
-		
 	}
 	for (var cell in cells ) {
 		cells[cell] = cells[cell].trim();
@@ -60,37 +52,15 @@ console.log("directions ", directions)
 	cells = cells.join("|");
 	function format(str, regx) {
 	  function replaceAndLog(match) { 
+
 	  	output[match] += 1;
 	    return '@';
 	  }
 	  return str.replace(regx, replaceAndLog);
 	}
 	cells = format(cells, overallRegx );
-	var newOutput = {};
-	var strip = /{|}/g;
-	for (var key in output) {
-		if (key === "wallHits") {
-			newOutput["wall hits"] = output[key];
-		}
-		else {
-			var newKey = key.replace(strip, "");
-			// console.log(newKey)
-			newOutput[newKey] = output[key];
-		}
-	
-	}
-	// console.log(newOutput)
-	
-	console.log( JSON.stringify(newOutput) );
-	if (cells){
-		console.log( cells )
-	} else {
-		console.log("no")
-	}
-	
-	
-	
-
+	console.log( JSON.stringify(output) );
+	cells ? console.log( cells ) : console.log("no");	
 }
 
 var input1 = [ 'right, up, up, down',
@@ -106,4 +76,4 @@ var input4 = [
 'qwekjs, asd{#}a, xnc{&}a',
 'qwekjs, asd{#}a, xnc{&}a'
 ]
-goshko(input4)
+goshko(input1)
--- exams/IV/rolland.js
+++ exams/IV/rolland.js
@@ -1,14 +1,15 @@
 function solve(arr) {
-	var players = {},
-	names,
-	scores, 
-	player1, player2,
-	scoresArray,
+	var playersArray = [], players,
 	regx = /\s+/g;
 
-	var Player = function() {
-		this.gamesLost = 0;
+	var Player = function(name) {
+		this.name = name
+		this.matchesWon = 0;
+		this.matchesLost = 0;
+		this.setsWon = 0;
+		this.setsLost = 0;
 		this.gamesWon = 0;
+		this.gamesLost = 0;
 	}
 			
 
@@ -18,7 +19,10 @@ function solve(arr) {
 	}
 
 	function formPlayers() {
-		var i, item;
+		var i, item, players = {},
+		names,
+		scores, 
+		player1, player2, scoresArray;
 
 		for (var i in arr) {
 			names = arr[i].split("vs.");
@@ -27,31 +31,103 @@ function solve(arr) {
 
 			player1 = trimStr(names[0]);
 			player2 = trimStr(scores[0]);
-		
-			players[player1] = new Player ;
-			players[player2] = new Player;
 
-			scoresArray = scores[1].split(" ").filter( function(el) {return el});
+			if ( !players[player1] ) {
+				players[player1] = new Player(player1);
+			}
+			if ( !players[player2] ) {
+				players[player2] = new Player(player2);
+			}
 			
-			console.log(scoresArray);
+			
+
+			scoresArray = scores[1].replace(regx, " ").split(" ").filter( function(el) {return el});
+			
+			// console.log(scoresArray);
+			var gamesWonPerPlayer1 = 0;
+
 			for (var item in scoresArray) {
-				console.log("item in scoresArray ",item)
-				var match = item.split("-");
-				console.log(match)
+				// console.log("item in scoresArray ", scoresArray[item])
+				var match = scoresArray[item].split("-");
+				// console.log("one match ", match);
+				var match1 = parseInt(match[0]);
+				var match2 = parseInt(match[1])
 				
-				players[player1].gamesWon += match[0];
-				players[player2].gamesWon += match[1];
+				players[player1].gamesWon += match1;
+				players[player2].gamesWon += match2;
+
+				players[player1].gamesLost += match2;
+				players[player2].gamesLost += match1;
+
+				if (match1 > match2) {
+
+				players[player1].setsWon += 1;
+				players[player2].setsLost +=1;
+				gamesWonPerPlayer1 ++;
+
+				} 
+				else if ( match1 < match2 ) {
+
+					players[player2].setsWon += 1;
+					players[player1].setsLost +=1;
+				}
 				
 			}
-
+			// console.log("at the end of iteration ", gamesWonPerPlayer1, scoresArray.length);
+			var player1Win = gamesWonPerPlayer1 >= Math.ceil(scoresArray.length/2)
+			if  ( player1Win ) {
+				players[player1].matchesWon +=1;
+				players[player2].matchesLost += 1;
+				
+			} else {
+				players[player2].matchesWon +=1;
+				players[player1].matchesLost += 1;
+			}
 
 		}
+		return players;
 
 	}
 
-	formPlayers()
-	console.log("players ", players)
+	players = formPlayers();
+	// console.log("players ", players)
+	for (var i in players) {
+		playersArray.push(players[i])
+		
+	}
+	
 
+	function compareMatches(a, b) {
+		if (a.matchesWon < b.matchesWon) {
+			return 1;
+		} else if (a.matchesWon > b.matchesWon) {
+			return -1;
+		} else {
+			if (a.setsWon < b.setsWon) {
+				return 1;
+			} else if (a.setsWon > b.setsWon) {
+				return -1;
+			}
+			else {
+				if (a.gamesWon < b.gamesWon) {
+					return 1;
+				} else if (a.gamesWon > b.gamesWon) {
+					return -1
+				} else {
+					if (a.name < b.name) {
+						return -1;
+					} else if (a.name > b.name) {
+						return 1;
+					} else {
+						return 0
+					}
+				}
+			}
+		}
+	}
+	playersArray.sort(compareMatches)
+	console.log(JSON.stringify(playersArray));
+	
 }
 
 
@@ -65,4 +141,30 @@ var input1 = ["Novak Djokovic vs. Roger Federer : 6-3 6-3",
 "Pete Sampras vs. Andre Agassi : 2-1",
 "Boris Beckervs.Andre        Agassi:2-1"]
 
-solve(input1)
+var input2 = ["Rafa Nadal vs. His Knees : 5-4 2-3 7-5 4-6 0-1",
+"Roger 		Federervs.His			Age:7-6 6-3 6-1",
+"HuanMartin  delPotro			vs.Wrist Injuries:		6-3		1-6 2-6",
+"WAYNE ODESNIK vs. Doping TwiceAlready             :0-6 0-6",
+"Rafa Nadal vs. His Knees : 5-4 1-3 7-5 4-6 0-1",
+"Roger 		Federervs.His			Age:7-6 6-3 6-1",
+"HuanMartin  delPotro			vs.Wrist Injuries:		6-3		1-6 2-6",
+"WAYNE ODESNIK vs. Doping TwiceAlready             :0-6 0-6 6-0 6-0 0-6",
+"Rafa Nadal vs. His Knees : 7-4 2-3 7-5 4-6 2-5",
+"Roger 		Federervs.His			Age:7-6 6-3 6-1",
+"HuanMartin  delPotro			vs.Wrist Injuries:		6-3		1-6 2-6",
+"WAYNE ODESNIK vs.Doping TwiceAlready             :0-6 0-6",
+"Rafa Nadalvs.His Knees : 5-3 2-3 7-1 4-6 0-1",
+"Roger Federer	vs.		His Age:7-6 6-3 6-1",
+"HuanMartin  delPotrn			vs.Wrist Injuries:		6-3		1-6 2-6",
+"WAYNE ODESNIK vs. Doping TwiceAlready             :0-6 0-6",
+"Rafa Nadal vs. His Knees : 5-4 2-3 7-5 4-6 0-1",
+"Roger 		Federervs.His			Age:7-6 6-3 6-1",
+"HuanMartin  delPotrn	  vs.Wrist Injuries:		6-3		1-6 2-6",
+"WAYNE ODESNIKvs. Doping TwiceAlready             :0-6 0-6",
+"Rafa Nadal vs. His Knees : 5-1 2-4 7-5 2-6 0-1",
+"Roger 		Federervs.His			Age:7-6 6-3 6-1",
+"HuanMartin  	delPotrn			vs.Wrist Injuries:		6-3 1-6 2-6",
+"WAYNE 	ODESNIK 	vs. Doping 		TwiceAlready	:1-6 2-6",
+]
+solve(input2)
+
