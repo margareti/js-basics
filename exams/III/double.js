bc0c48f34ffa795004ad2a0ff8116a409c78e224
Margarita Ignatieva <margaret.vegan@gmail.com>
2016-02-02 14:40:16 +0200

    exam III ex 1 and 2

--- exams/III/double.js
+++ exams/III/double.js
@@ -4,19 +4,36 @@ function solve(arr){
 		num = 'num';
 
 		function isRakiya(num){
-			return false;
+			if (num < 1000) {
+				return false;
+			}
+			var strNum = num.toString();
+			//console.log(strNum)			
+			for (var i = 1; i < strNum.length; i ++) {
+				var current = strNum[i],
+					prev = strNum[i-1],
+					winner = prev + current,
+					strCheck = strNum.substring(i + 1);
+
+					//console.log("winner ", winner, "string ", strCheck)
+					if (strCheck.indexOf(winner) != -1) {
+						return true;
+					}					
+			}
 		}
 
 		console.log("<ul>");
 		for (var i = start; i <= end; i ++){
 			var liStart = '<li>',
-				liEnd = '</li>'
-			var line = "<span class='" + num + "'>" + i + '</span>';
+				liEnd = '</li>',
+				line;
 
 			if (isRakiya(i)) {
 				var link = '<a href="view.php?id=' + i + '">View</a>';
+				line = "<span class='rakiya'>" + i + '</span>';
 				console.log(liStart + line + link + liEnd);
 			} else {
+				line = "<span class='" + num + "'>" + i + '</span>';
 				console.log(liStart + line + liEnd);
 			}
 			
@@ -31,5 +48,5 @@ var input1 = [ '5', '8' ],
 
 
 
-solve(input1)
+solve(input3)
 
